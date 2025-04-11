e5fb08e53d895796f7912525922ebd8e3d8ad2f5
Lukas Gobelet <lulugo19@posteo.net>
2025-04-11 20:57:57 +0200

    nullable literal

--- src/Token.cs
+++ src/Token.cs
@@ -2,10 +2,10 @@ public class Token
 {
     public TokenType Type {get; init;}
     public string Lexeme {get; init;}
-    public object Literal {get; init;}
+    public object? Literal {get; init;}
     public int Line {get; init;}
 
-    public Token(TokenType type, string lexeme, Object literal, int line)
+    public Token(TokenType type, string lexeme, object? literal, int line)
     {
         Type = type;
         Lexeme = lexeme;
@@ -15,6 +15,6 @@ public class Token
 
     public override String ToString()
     {
-        return Type + " " + Lexeme + " " + Literal.ToString();
+        return Type + " " + Lexeme + " " + (Literal ?? "null");
     }
 }
